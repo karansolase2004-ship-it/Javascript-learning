8a21319d3cbd1e5b7fedcd3eed60d57bfb202058
karansolase <126546501+karansolase@users.noreply.github.com>
2026-08-24 20:16:05 +0530

    Branching looping STatement in JS

---
+++ 4_branching_and_Control_Flow/1_loops.js
@@ -0,0 +1,16 @@
+//  Early return pattern 
+// remember to write break in switch casellllllllll
+
+let v = prompt("Give a number : ");
+
+for(let i=1; i<11; ++i)
+{
+    console.log(`5 * ${i} = ${5*i}`);
+}
+
+let i = 1;
+while(i<30)
+{
+    console.log(i);
+    ++i;
+}
