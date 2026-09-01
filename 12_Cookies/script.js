1ced9b657a43259058a4c820828944742915ab0c
karansolase <126546501+karansolase@users.noreply.github.com>
2026-09-01 16:03:51 +0530

    Cookie

---
+++ 12_Cookies/script.js
@@ -0,0 +1,3 @@
+// Cookies : 4kb, Can expire Automaticallly or when the he browser closes (session cookies), JavaScript (unless HttpOnly) and the server, Authentication, session management, user tracking
+
+// local Storage : 5-10mb, persist until removed,JavaScript (localStorage), User preferences, theme, cart, app data
